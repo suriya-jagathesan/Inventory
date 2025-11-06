304a2e72797eb765ab5e2f4b0926f613a3c908b0
chakkaravarthyJs <surya@bizappln.com>
2025-11-06 21:51:20 +0530

    Initiall Commit

---
+++ src/main/java/com/example/Inventory_Management/Inventory/Management/entity/Role.java
@@ -0,0 +1,14 @@
+package com.example.Inventory_Management.Inventory.Management.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name = "role")
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String roleName;
+}
---
+++ src/main/java/com/example/Inventory_Management/Inventory/Management/entity/User.java
@@ -0,0 +1,28 @@
+package com.example.Inventory_Management.Inventory.Management.entity;
+
+import jakarta.persistence.*;
+import jdk.jfr.Enabled;
+import lombok.Data;
+
+import java.util.Set;
+
+@Enabled
+@Data
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+    private String password;
+    private boolean enabled = true;
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(
+            name = "user_roles",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id"))
+    private Set<Role> roles;
+
+}
